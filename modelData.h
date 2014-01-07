97c973620bd52307b08afeba5ad0588a494fd4d5
Alec Bennett <alec@alecbennett.com>
2014-01-06 18:48:00 -0900

    Adding basic getter/setter

--- modelData.h
+++ modelData.h
@@ -2,11 +2,16 @@
 #define MODELDATA_H
 
 template<class T> class modelData {
-        T dataArray;
+        T **dataArray;
 public:
         modelData() : dataArray( T() ){
-
         }
+	void setValue( T x, T y, T val ){
+
+	}
+	T getValue( T x, T y ){
+		return dataArray[x][y];
+	}
 };
 
 #endif
