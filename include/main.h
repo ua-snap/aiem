1a8daa1880b3dfde604af253122c7604e383fe8b
Alec Bennett <alec@alecbennett.com>
2017-10-16 14:56:11 -0800

    Added main.h

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "ArgParse.h"
+#include "aiem.h"
+#include "alfresco.h"
+#include "GIPL2.h"
+#include "dvmdostem.h"
+#include "TEMUtilityFunctions.h"
+
+#endif
